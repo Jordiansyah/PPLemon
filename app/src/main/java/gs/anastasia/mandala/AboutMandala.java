93ab2de4a6d911a9719c4a43fd0f8a278a5d930a
Jordi <muhammad.jordiansyah@gmail.com>
2019-03-23 22:14:11 +0700

    Creating Mandala Project

---
+++ app/src/main/java/gs/anastasia/mandala/AboutMandala.java
@@ -0,0 +1,13 @@
+package gs.anastasia.mandala;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutMandala extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_mandala);
+    }
+}
---
+++ app/src/main/java/gs/anastasia/mandala/MainMenu.java
@@ -0,0 +1,26 @@
+package gs.anastasia.mandala;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+
+    Button StartButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        StartButton = (Button) findViewById(R.id.startButton);
+
+        StartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainMenu.this, SelectionMenu.class));
+            }
+        });
+    }
+}
